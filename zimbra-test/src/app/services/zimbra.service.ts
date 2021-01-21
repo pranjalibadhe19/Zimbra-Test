f1ffdf6955767499c2a0ef54182192b7d3c2e812
Pranjali Rane <pranjalibadhe19@gmail.com>
2021-01-21 22:23:27 +0530

    added aws instance ip

--- zimbra-test/src/app/services/zimbra.service.ts
+++ zimbra-test/src/app/services/zimbra.service.ts
@@ -8,19 +8,19 @@ export class ZimbraService {
   constructor(private http: HttpClient) {}
 
   getCategoriesList() {
-    return this.http.get('http://localhost:3000/categories');
+    return this.http.get('http://13.234.33.140:3000/categories');
   }
   
   getCategoryById(categoryId) {
-    return this.http.get('http://localhost:3000/categories?id='+categoryId);
+    return this.http.get('http://13.234.33.140:3000/categories?id='+categoryId);
   }
   
   getProductsList(categoryId) {
-    return this.http.get('http://localhost:3000/products?categoryId='+categoryId);
+    return this.http.get('http://13.234.33.140:3000/products?categoryId='+categoryId);
   }
   
   getProductById(productId) {
-    return this.http.get('http://localhost:3000/products?id='+productId);
+    return this.http.get('http://13.234.33.140:3000/products?id='+productId);
   }
 
 }
